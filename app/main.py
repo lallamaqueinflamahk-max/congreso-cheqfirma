1437f5a6403074221954f323b664821228cd83a2
lallamaqueinflamahk-max <lallamaqueinflamahk@gmail.com>
2026-04-14 17:31:09 -0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {
+        "status": "online",
+        "proyecto": "Congreso Cheqfirma",
+        "mensaje": "Si ves esto, el deploy fue exitoso"
+    }
+
+@app.get("/test")
+def test():
+    return {"message": "Ruta de prueba funcionando"}
